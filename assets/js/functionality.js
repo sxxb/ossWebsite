cc8d950c7de650542fdbf66588be332e71ae5d39
sxxb <scottbelcher@live.com.au>
2020-11-30 13:54:31 +1030

    adjusting functionality

--- assets/js/functionality.js
+++ assets/js/functionality.js
@@ -33,7 +33,10 @@ $(document).ready(function() {
     $(`.header-nav-main-link-top-link`).click(function() {
       if($(window).width() < 600) {
         event.preventDefault();
-        $(`.header-nav-main-link-top-link`).siblings(`.header-nav-main-link-dropdown-list`).slideToggle(250);
+        $(`.header-nav-main-link-top-link`).siblings(`.header-nav-main-link-dropdown-list`).
+        .not($(this).next(`.header-nav-main-link-dropdown-list`)
+        .slideUp(500);
+        $(this).next(`.header-nav-main-link-dropdown-list`).slideToggle(500);
       }
     });
 });
