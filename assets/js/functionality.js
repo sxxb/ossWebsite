097160d88aa5d68aedac48c443db3548cc84a257
sxxb <scottbelcher@live.com.au>
2020-11-30 13:56:22 +1030

    fixed missing parenthesis

--- assets/js/functionality.js
+++ assets/js/functionality.js
@@ -34,7 +34,7 @@ $(document).ready(function() {
       if($(window).width() < 600) {
         event.preventDefault();
         $(`.header-nav-main-link-top-link`).siblings(`.header-nav-main-link-dropdown-list`).
-        .not($(this).next(`.header-nav-main-link-dropdown-list`)
+        .not($(this).next(`.header-nav-main-link-dropdown-list`))
         .slideUp(500);
         $(this).next(`.header-nav-main-link-dropdown-list`).slideToggle(500);
       }
