822a0b9bdf4e13c47ea7c95d5b6f2cbd24d3329b
sxxb <scottbelcher@live.com.au>
2021-11-29 15:57:08 +1030

    search functionality test 2

--- assets/js/functionality.js
+++ assets/js/functionality.js
@@ -26,14 +26,12 @@ $(function(){
        $(`body`).css({'overflow': 'auto'});
        }
      });
-     $(function() {
-       $("#searchField").on("Keyup", function() {
-         var searchTerm = $(this).val().toLowerCase();
-         $("#searchList li").filter(function() {
-          $(this).toggle($(this).text().toLowerCase().indexOf(searchTerm) > -1)
-        });
-      });
-    });
+     $("#searchField").on("Keyup", function() {
+       var searchTerm = $(this).val().toLowerCase();
+       $("#searchList li").filter(function() {
+         $(this).toggle($(this).text().toLowerCase().indexOf(searchTerm) > -1)
+       });
+     });
   });
   $("#contactForm").load("partials/contact.html", );
   $("#footer").load("partials/footer.html");
