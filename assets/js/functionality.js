cf59b876aa1c776a6a134ec2c16e3f02cf6623c1
sxxb <scottbelcher@live.com.au>
2021-11-29 14:43:02 +1030

    implementing partials

--- assets/js/functionality.js
+++ assets/js/functionality.js
@@ -1,39 +1,5 @@
-//jquery includes
-
-
-// //Mobile Menu Open
-// $(function() {
-//   $(`#menuTrigger`).click(function() {
-//     if ($(window).width() < 600) {
-//       $(`.header-nav`).removeClass(`header-nav-closed`).addClass(`header-nav-open`);
-//       $(`body`).css({'overflow': 'hidden'});
-//     }
-//    });
-//    $(`#menuExit`).click(function() {
-//      $(`.header-nav`).removeClass(`header-nav-open`).addClass(`header-nav-closed`);
-//      $(`.header-nav-main-link-dropdown-list`).removeAttr("style");
-//      $(`body`).css({'overflow': 'auto'});
-//    });
-//    $(`.header-nav-main-link-top-link`).click(function() {
-//      if($(window).width() < 600) {
-//        event.preventDefault();
-//        $(`.header-nav-main-link-top-link`).siblings(`.header-nav-main-link-dropdown-list`)
-//        .not($(this).next(`.header-nav-main-link-dropdown-list`))
-//        .slideUp(500);
-//        $(this).next(`.header-nav-main-link-dropdown-list`).slideToggle(500);
-//      }
-//    });
-//    $(window).resize(function() {
-//      if ($(window).width() >= 600) {
-//      $(`.header-nav-main-link-dropdown-list`).removeAttr("style");
-//      $(`body`).css({'overflow': 'auto'});
-//      }
-//    });
-// });
-
 $(function(){
-  $("#nav").load("partials/nav.html");
-  $("#contactForm").load("partials/contact.html", function() {
+  $("#nav").load("partials/nav.html", function() {
     $(`#menuTrigger`).click(function() {
       if ($(window).width() < 600) {
         $(`.header-nav`).removeClass(`header-nav-closed`).addClass(`header-nav-open`);
@@ -61,16 +27,7 @@ $(function(){
        }
      });
   });
+  $("#contactForm").load("partials/contact.html", );
   $("#footer").load("partials/footer.html");
   console.log('hello world');
 });
-
-// //Mobile Menu Dropdown
-// $(document).ready(function() {
-//
-// });
-//
-// //Screensize Menu Fix
-// $(document).ready(function() {
-//
-// });
