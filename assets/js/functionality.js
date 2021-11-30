885d7ca8e1407421a0d2a3b26dfda40ff1d4849e
sxxb <scottbelcher@live.com.au>
2021-11-30 13:40:49 +1030

    head matter insertion test 0.3

--- assets/js/functionality.js
+++ assets/js/functionality.js
@@ -1,18 +1,22 @@
 $(function(){
-  $('head').append(
-    '<meta name="theme-color" content="#31aa62"><meta name="description" content="OSS provide Electrical Testing and Other Safety Services to South Australia."><meta name="robots" content="index,follow"><meta name="googlebot" content="index,follow"><link rel="stylesheet" href="assets/css/style.css" /><link rel="shortcut icon" type="image/png" href="img/favicon.ico" />'
+  $(`head`).append(
+    `<meta name="theme-color" content="#31aa62">`,
+    `<meta name="description" content="OSS provide Electrical Testing and Other Safety Services to South Australia."><meta name="robots" content="index,follow">`,
+    `<meta name="googlebot" content="index,follow">`,
+    `<link rel="stylesheet" href="assets/css/style.css" />`,
+    `<link rel="shortcut icon" type="image/png" href="img/favicon.ico" />`
   );
-  $("#nav").load("partials/nav.html", function() {
+  $(`#nav`).load(`partials/nav.html`, function() {
     $(`#menuTrigger`).click(function() {
       if ($(window).width() < 600) {
         $(`.header-nav`).removeClass(`header-nav-closed`).addClass(`header-nav-open`);
-        $(`body`).css({'overflow': 'hidden'});
+        $(`body`).css({`overflow`: `hidden`});
       }
      });
     $(`#menuExit`).click(function() {
        $(`.header-nav`).removeClass(`header-nav-open`).addClass(`header-nav-closed`);
-       $(`.header-nav-main-link-dropdown-list`).removeAttr("style");
-       $(`body`).css({'overflow': 'auto'});
+       $(`.header-nav-main-link-dropdown-list`).removeAttr(`style`);
+       $(`body`).css({`overflow`: `auto`});
      });
     $(`.header-nav-main-link-top-link`).click(function() {
        if($(window).width() < 600) {
@@ -25,35 +29,35 @@ $(function(){
      });
     $(window).resize(function() {
        if ($(window).width() >= 600) {
-       $(`.header-nav-main-link-dropdown-list`).removeAttr("style");
-       $(`body`).css({'overflow': 'auto'});
+       $(`.header-nav-main-link-dropdown-list`).removeAttr(`style`);
+       $(`body`).css({`overflow`: `auto`});
        }
      });
-    $("#searchField").on("keyup", function() {
+    $(`#searchField`).on(`keyup`, function() {
        var searchTerm = $(this).val().toLowerCase();
-       $("#searchList li").filter(function() {
+       $(`#searchList li`).filter(function() {
          $(this).toggle($(this).text().toLowerCase().indexOf(searchTerm) > -1)
        });
      });
-    $("#searchField").focusout(function() {
-      if ($("#searchField").parent().contents().not(':focus')) {
+    $(`#searchField`).focusout(function() {
+      if ($(`#searchField`).parent().contents().not(`:focus`)) {
 
     }
     else {
-      $("#searchList li").hide();
+      $(`#searchList li`).hide();
     }
   });
-    $("#searchField").focusin(function() {
+    $(`#searchField`).focusin(function() {
       var searchTerm = $(this).val().toLowerCase();
-      if (searchTerm != '') {
-      $("#searchList li").filter(function() {
+      if (searchTerm != ``) {
+      $(`#searchList li`).filter(function() {
         $(this).toggle($(this).text().toLowerCase().indexOf(searchTerm) > -1)
       });
     }
    });
   });
-  $("#contactForm").load("partials/contact.html", function() {
+  $(`#contactForm`).load(`partials/contact.html`, function() {
 
   });
-  $("#footer").load("partials/footer.html");
+  $(`#footer`).load(`partials/footer.html`);
 });
