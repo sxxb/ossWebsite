e4fecb77d92300f8f31bea38b24344d34ec3ca01
sxxb <scottbelcher@live.com.au>
2021-12-16 14:51:34 +1030

    contact form implementation

--- partials/contact.php
+++ partials/contact.php
@@ -1,5 +1,69 @@
 <?php
+echo '
+<div class="quote-title">
+ <h2>Get a Quote</h2>
+</div>
+<div class="quote-form">
+ <form id="contactForm" action="" method="POST">
+   <div>
+     <fieldset>
+       <input type="text" id="quoteFormName" placeholder="Name" name="contactName" required>
+       <input type="text" id="quoteFormBusinessName" placeholder="Business Name" name="contactBusinessName" required>
+       <input type="text" id="quoteFormAddress" placeholder="Address" name="contactAddress" required>
+       <input type="tel"  id="quoteFormPhone" placeholder="Phone" name="contactPhone" required>
+       <input type="email"  id="quoteFormEmail" placeholder="Email" name="contactEmail" required>
+     </fieldset>
+     <fieldset>
+       <select id="quoteFormHearAbout" name="contactHearAbout" required>
+         <option value="" class="option-placeholder" disabled selected>How did you hear about OSS?</option>
+         <option value="Business Contact">Business Contact</option>
+         <option value="Google Search">Google Search</option>
+         <option value="Google Ads">Google Ads</option>
+         <option value="Other Search Engine">Other Search Engine</option>
+         <option value="Family/Friends">Family/Friends</option>
+         <option value="Exisiting Customer">Exisiting Customer</option>
+         <option value="Promotion">Promotion</option>
+         <option value="Social Network">Social Network</option>
+         <option value="Facebook Ad">Facebook Ad</option>
+         <option value="Trade Show">Trade Show</option>
+         <option value="Brochure">Brochure</option>
+         <option value="Others">Others</option>
+       </select>
+       <select id="quoteFormEnquiryType" name="contactEnquiryType" required>
+         <option value="" class="option-placeholder" disabled selected>Service Required</option>
+         <option value="Service Visit">Service Visit</option>
+         <option value="Appliance Testing">Appliance Testing</option>
+         <option value="RCD Testing">RCD Testing</option>
+         <option value="Emergency Exit/Light Testing">Emergency Exit/Light Testing</option>
+         <option value="Thermography">Thermography</option>
+         <option value="LED Lighting">LED Lighting</option>
+         <option value="Electrical Maintenance">Electrical Maintenance</option>
+         <option value="Solar Panel Installations">Solar Panel Installations</option>
+         <option value="Others">Others</option>
+       </select>
+       <textarea id="quoteFormQuoteRequest" name="contactQuoteRequest" rows="5" placeholder="Quote Request Description"></textarea>
+     </fieldset>
+   </div>
+   <div>
+     <input
+       type="submit"
+       value="Request Quote"
+       class="g-recaptcha"
+       data-sitekey="6LcDGGodAAAAAPh66Ioy4kdhL1iDDf_iXAE1GZpP"
+       data-callback="onSubmit"
+       data-action="submit"
+       >
+   </div>
+
+
+
+
+ </form>
+';
+
+
 if ($_POST) {
+  $contactDate = "";
   $contactName = "";
   $contactBusinessName = "";
   $contactAddress = "";
@@ -8,6 +72,9 @@ if ($_POST) {
   $contactHearAbout = "";
   $contactEnquiryType = "";
   $contactQuoteRequest = "";
+
+  date_default_timezone_set("Australia/Adelaide");
+  $contactDate = date("h:i A, d/m/Y");
 
   if(isset($_POST['contactName'])) {
     $contactName = filter_var($_POST['contactName'], FILTER_SANITIZE_STRING);
@@ -37,18 +104,24 @@ if ($_POST) {
 
   $email_from  = 'quoterequest@occsafetyservices.com.au';
   $email_subject = "Quote Request - $contactBusinessName";
-  $email_body = "Contact Name: $contactName.\n".
-                "Business: $contactBusinessName.\n".
-                "Address: $contactAddress.\n".
-                "Phone: $contactPhone.\n".
-                "Email: $contactEmail.\n".
-                "Enquiry Type: $contactEnquiryType.\n".
-                "Message: $contactQuoteRequest.\n".
-                "This person heard about OSS from: $contactHearAbout.\n";
+  $email_body = "<div><em style='font-size:0.8em'>Quote requested at $contactDate</em></div>\n".
+                "<div><strong>Contact Name:</strong> $contactName </div>\n".
+                "<div><strong>Business:</strong> $contactBusinessName </div>\n".
+                "<div><strong>Address:</strong> $contactAddress </div>\n".
+                "<div><strong>Phone:</strong> $contactPhone </div>\n".
+                "<div><strong>Email:</strong> $contactEmail </div>\n".
+                "<br/>\n".
+                "<div><strong>Enquiry Type:</strong> $contactEnquiryType </div>\n".
+                "<div><strong>Message:</strong> </div>\n".
+                "<div>$contactQuoteRequest </div>\n".
+                "<br/>\n".
+                "<div><strong>This person heard about OSS from:</strong> $contactHearAbout </div>\n";
   $email_to = "graphics@wavecom.com.au";
 
-  $headers  = 'From: ' . $email_from . "\r\n" .
-              'Reply-To ' . $contactEmail;
+  $headers  = 'MIME-Version: 1.0' . "\r\n".
+              'Content-type: text/html; charset=utf-8' . "\r\n".
+              'From: ' . $email_from . "\r\n" .
+              'Reply-To: ' . $contactEmail;
 
   // $secretKey = "6LcDGGodAAAAAAn1yEZKDMkKn2kUoZZMRTyln0nr";
   // $responseKey = $_POST['g-recaptcha-response'];
@@ -62,72 +135,19 @@ if ($_POST) {
   // if ($response->success)
   // {
     mail($email_to, $email_subject, $email_body, $headers);
-    echo "Quote Request Sent Successfully";
+    echo "
+      <div class='quote-form-notice quote-form-notice-good'>
+        Quote Request Sent Successfully
+      </div>
+      ";
   // }
   // else {
-  //   echo "Oops, please try again";
+  //   echo "
+  //    <div class='quote-form-notice quote-form-notice-bad'>
+  //      Oops, please try again";
+  //     </div>
   // }
 };
 
- echo '
-<div class="quote-title">
-  <h2>Get a Quote</h2>
-</div>
-<div class="quote-form">
-  <form id="contactForm" action="" method="POST">
-    <div>
-      <fieldset>
-        <input type="text" id="quoteFormName" placeholder="Name" name="name" required>
-        <input type="text" id="quoteFormBusinessName" placeholder="Business Name" name="businessName" required>
-        <input type="text" id="quoteFormAddress" placeholder="Address" name="address" required>
-        <input type="tel"  id="quoteFormPhone"placeholder="Phone" name="phone" required>
-        <input type="email"  id="quoteFormEmail" placeholder="Email" name="email" required>
-      </fieldset>
-      <fieldset>
-        <select id="quoteFormHearAbout" name="hearAbout" required>
-          <option value="" class="option-placeholder" disabled selected>How did you hear about OSS?</option>
-          <option value="Business Contact">Business Contact</option>
-          <option value="Google Search">Google Search</option>
-          <option value="Google Ads">Google Ads</option>
-          <option value="Other Search Engine">Other Search Engine</option>
-          <option value="Family/Friends">Family/Friends</option>
-          <option value="Exisiting Customer">Exisiting Customer</option>
-          <option value="Promotion">Promotion</option>
-          <option value="Social Network">Social Network</option>
-          <option value="Facebook Ad">Facebook Ad</option>
-          <option value="Trade Show">Trade Show</option>
-          <option value="Brochure">Brochure</option>
-          <option value="Others">Others</option>
-        </select>
-        <select id="quoteFormEnquiryType" name="enquiryType" required>
-          <option value="" class="option-placeholder" disabled selected>Service Required</option>
-          <option value="Service Visit">Service Visit</option>
-          <option value="Appliance Testing">Appliance Testing</option>
-          <option value="RCD Testing">RCD Testing</option>
-          <option value="Emergency Exit/Light Testing">Emergency Exit/Light Testing</option>
-          <option value="Thermography">Thermography</option>
-          <option value="LED Lighting">LED Lighting</option>
-          <option value="Electrical Maintenance">Electrical Maintenance</option>
-          <option value="Solar Panel Installations">Solar Panel Installations</option>
-          <option value="Others">Others</option>
-        </select>
-        <textarea id="quoteFormQuoteRequest" name="quoteRequest" rows="5" placeholder="Quote Request Description"></textarea>
-      </fieldset>
-    </div>
-    <div>
-      <input
-        type="submit"
-        value="Request Quote"
-        class="g-recaptcha"
-        data-sitekey="6LcDGGodAAAAAPh66Ioy4kdhL1iDDf_iXAE1GZpP"
-        data-callback="onSubmit"
-        data-action="submit"
-        >
-    </div>
 
-
-
-
-  </form>
-'
 ?>
--- partials/headmatter.php
+++ partials/headmatter.php
@@ -4,7 +4,8 @@
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
-<base href="//localhost/ossWebsite/" target="_self">
+<base href="https://localhost/ossWebsite/" target="_self">
+<!-- <base href="https://www.occsafetyservices.com/_testing/" target="_self"> -->
 
 
 <meta name="theme-color" content="#31aa62">
