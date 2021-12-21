0f6bfad3d8f6a57827d296ce77a8577b34e599f7
sxxb <scottbelcher@live.com.au>
2021-12-21 14:37:30 +1030

    contact form error handling

--- partials/contact.php
+++ partials/contact.php
@@ -1,20 +1,137 @@
 <?php
-echo '
+date_default_timezone_set("Australia/Adelaide");
+
+$contactName = "";
+$contactBusinessName = "";
+$contactAddress = "";
+$contactPhone = "";
+$contactEmail = "";
+$contactHearAbout = "";
+$contactEnquiryType = "";
+$contactQuoteRequest = "";
+
+$contactNameError = $contactBusinessNameError = $contactAddressError = $contactPhoneError = $contactEmailError = $contactQuoteRequestError = $contactHearAboutError = $contactEnquiryTypeError = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+  $contactDate = date("h:i A, d/m/Y");
+
+  function clean_input($data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = filter_var($data, FILTER_SANITIZE_STRING);
+    $data = htmlspecialchars($data);
+    return $data;
+  }
+
+    $contactName = clean_input($_POST["contactName"]);
+    $contactBusinessName = clean_input($_POST["contactBusinessName"]);
+    $contactAddress = clean_input($_POST["contactAddress"]);
+    $contactPhone = clean_input($_POST["contactPhone"]);
+    $contactHearAbout = $_POST["contactHearAbout"];
+    $contactEnquiryType = $_POST["contactEnquiryType"];
+    $contactQuoteRequest = clean_input($_POST["contactQuoteRequest"]);
+
+    $contactEmail = str_replace(array("\r", "\n", "%0a", "%0d"), '', $_POST['contactEmail']);
+
+  if (empty($contactName)) {
+    $contactNameError = "Required";
+    echo "
+     <div class='quote-form-notice quote-form-notice-bad'>
+       Oops, you forgot something - please try again
+      </div>
+      ";
+if (empty($contactBusinessName)) {
+    $contactBusinessNameError = "Required";
+  if (empty($contactAddress)) {
+    $contactAddressError = "Required";
+  if (empty($contactPhone)) {
+    $contactPhoneError = "Required";
+  // if (empty($contactHearAbout)) {
+  //   $contactHearAboutError = "Required";
+  // if (empty($contactEnquiryType)) {
+  //   $contactEnquiryTypeError = "Required";
+  if (empty($contactQuoteRequest)) {
+    $contactQuoteRequestError = "Required";
+  if (empty($contactEmail)) {
+    $contactEmailError = "Required";
+  if (!filter_var($contactEmail, FILTER_VALIDATE_EMAIL)) {
+    $contactEmailError = "Required";
+  }}}
+// }}
+}}}}
+
+  else {
+    $email_from  = 'quoterequest@occsafetyservices.com.au';
+    $email_subject = "Quote Request - $contactBusinessName";
+    $email_body = "<div><em style='font-size:0.8em'>Quote requested at $contactDate</em></div>\n".
+                  "<div><strong>Contact Name:</strong> $contactName </div>\n".
+                  "<div><strong>Business:</strong> $contactBusinessName </div>\n";
+                  "<div><strong>Address:</strong> $contactAddress </div>\n".
+                  "<div><strong>Phone:</strong> $contactPhone </div>\n".
+                  "<div><strong>Email:</strong> $contactEmail </div>\n".
+                  "<br/>\n".
+                  "<div><strong>Enquiry Type:</strong> $contactEnquiryType </div>\n".
+                  "<div><strong>Message:</strong> </div>\n".
+                  "<div>$contactQuoteRequest </div>\n".
+                  "<br/>\n".
+                  "<div><strong>This person heard about OSS from:</strong> $contactHearAbout </div>\n";
+    $email_to = "graphics@wavecom.com.au";
+
+    $headers  = 'MIME-Version: 1.0' . "\r\n".
+                'Content-type: text/html; charset=utf-8' . "\r\n".
+                'From: ' . $email_from . "\r\n" .
+                'Reply-To: ' . $contactEmail;
+
+    $secretKey = "6LcDGGodAAAAAAn1yEZKDMkKn2kUoZZMRTyln0nr";
+    $responseKey = $_POST['g-recaptcha-response'];
+    $UserIP = $_SERVER['REMOTE_ADDR'];
+    $url = "https://www.google.com/recaptcha/api/siteverify?secret=".$secretKey."&response=".$responseKey."&remoteIP=".$UserIP;
+
+    $response = file_get_contents($url);
+    $response = json_decode($response);
+
+
+
+    if ($response->success)
+    {
+      // mail($email_to, $email_subject, $email_body, $headers);
+      echo "$email_to, $email_subject, $email_body, $headers";
+      echo "
+        <div class='quote-form-notice quote-form-notice-good'>
+          Quote Request Sent Successfully
+        </div>
+        ";
+    }
+    else {
+      echo "
+       <div class='quote-form-notice quote-form-notice-bad'>
+         Oops, something went wrong - please try again
+        </div>
+        ";
+    }
+  }
+
+};
+
+
+?>
+
 <div class="quote-title">
  <h2>Get a Quote</h2>
 </div>
 <div class="quote-form">
- <form id="contactForm" action="" method="POST">
+ <form id="contactForm" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="POST">
    <div>
      <fieldset>
-       <input type="text" id="quoteFormName" placeholder="Name" name="contactName" required>
-       <input type="text" id="quoteFormBusinessName" placeholder="Business Name" name="contactBusinessName" required>
-       <input type="text" id="quoteFormAddress" placeholder="Address" name="contactAddress" required>
-       <input type="tel"  id="quoteFormPhone" placeholder="Phone" name="contactPhone" required>
-       <input type="email"  id="quoteFormEmail" placeholder="Email" name="contactEmail" required>
-     </fieldset>
+       <input type="text" id="quoteFormName" placeholder="Name" name="contactName" required><span class="error"><?php echo $contactNameError;?></span>
+       <input type="text" id="quoteFormBusinessName" placeholder="Business Name" name="contactBusinessName"><span class="error"><?php echo $contactBusinessNameError;?></span>
+       <input type="text" id="quoteFormAddress" placeholder="Address" name="contactAddress" required><span class="error"><?php echo $contactAddressError;?></span>
+       <input type="tel"  id="quoteFormPhone" placeholder="Phone" name="contactPhone" required><span class="error"><?php echo $contactPhoneError;?></span>
+       <input type="email"  id="quoteFormEmail" placeholder="Email" name="contactEmail" required><span class="error"><?php echo $contactEmailError;?>
+     </fieldset></span>
      <fieldset>
-       <select id="quoteFormHearAbout" name="contactHearAbout" required>
+       <select id="quoteFormHearAbout" name="contactHearAbout" required><span class="error"><?php echo $contactHearAboutError;?></span>
          <option value="" class="option-placeholder" disabled selected>How did you hear about OSS?</option>
          <option value="Business Contact">Business Contact</option>
          <option value="Google Search">Google Search</option>
@@ -29,7 +146,7 @@ echo '
          <option value="Brochure">Brochure</option>
          <option value="Others">Others</option>
        </select>
-       <select id="quoteFormEnquiryType" name="contactEnquiryType" required>
+       <select id="quoteFormEnquiryType" name="contactEnquiryType" required><span class="error"><?php echo $contactEnquiryTypeError;?></span>
          <option value="" class="option-placeholder" disabled selected>Service Required</option>
          <option value="Service Visit">Service Visit</option>
          <option value="Appliance Testing">Appliance Testing</option>
@@ -41,7 +158,7 @@ echo '
          <option value="Solar Panel Installations">Solar Panel Installations</option>
          <option value="Others">Others</option>
        </select>
-       <textarea id="quoteFormQuoteRequest" name="contactQuoteRequest" rows="5" placeholder="Quote Request Description"></textarea>
+       <textarea id="quoteFormQuoteRequest" name="contactQuoteRequest" rows="5" placeholder="Quote Request Description"></textarea><span class="error"><?php echo $contactQuoteRequestError;?>
      </fieldset>
    </div>
    <div>
@@ -54,97 +171,4 @@ echo '
        data-action="submit"
        >
    </div>
-
-
-
-
  </form>
-';
-
-
-if ($_POST) {
-  $contactDate = "";
-  $contactName = "";
-  $contactBusinessName = "";
-  $contactAddress = "";
-  $contactPhone = "";
-  $contactEmail = "";
-  $contactHearAbout = "";
-  $contactEnquiryType = "";
-  $contactQuoteRequest = "";
-
-  date_default_timezone_set("Australia/Adelaide");
-  $contactDate = date("h:i A, d/m/Y");
-
-  if(isset($_POST['contactName'])) {
-    $contactName = filter_var($_POST['contactName'], FILTER_SANITIZE_STRING);
-  }
-  if(isset($_POST['contactBusinessName'])) {
-    $contactBusinessName = filter_var($_POST['contactBusinessName'], FILTER_SANITIZE_STRING);
-  }
-  if(isset($_POST['contactAddress'])) {
-    $contactAddress = filter_var($_POST['contactAddress'], FILTER_SANITIZE_STRING);
-  }
-  if(isset($_POST['contactPhone'])) {
-    $contactPhone = filter_var($_POST['contactPhone'], FILTER_SANITIZE_STRING);
-  }
-  if(isset($_POST['contactHearAbout'])) {
-    $contactHearAbout = filter_var($_POST['contactHearAbout'], FILTER_SANITIZE_STRING);
-  }
-  if(isset($_POST['contactEnquiryType'])) {
-    $contactEnquiryType = filter_var($_POST['contactEnquiryType'], FILTER_SANITIZE_STRING);
-  }
-  if(isset($_POST['contactQuoteRequest'])) {
-    $contactQuoteRequest = htmlspecialchars($_POST['contactQuoteRequest']);
-  }
-  if(isset($_POST['contactEmail'])) {
-      $contactEmail = str_replace(array("\r", "\n", "%0a", "%0d"), '', $_POST['contactEmail']);
-      $contactEmail = filter_var($contactEmail, FILTER_VALIDATE_EMAIL);
-    }
-
-  $email_from  = 'quoterequest@occsafetyservices.com.au';
-  $email_subject = "Quote Request - $contactBusinessName";
-  $email_body = "<div><em style='font-size:0.8em'>Quote requested at $contactDate</em></div>\n".
-                "<div><strong>Contact Name:</strong> $contactName </div>\n".
-                "<div><strong>Business:</strong> $contactBusinessName </div>\n".
-                "<div><strong>Address:</strong> $contactAddress </div>\n".
-                "<div><strong>Phone:</strong> $contactPhone </div>\n".
-                "<div><strong>Email:</strong> $contactEmail </div>\n".
-                "<br/>\n".
-                "<div><strong>Enquiry Type:</strong> $contactEnquiryType </div>\n".
-                "<div><strong>Message:</strong> </div>\n".
-                "<div>$contactQuoteRequest </div>\n".
-                "<br/>\n".
-                "<div><strong>This person heard about OSS from:</strong> $contactHearAbout </div>\n";
-  $email_to = "graphics@wavecom.com.au";
-
-  $headers  = 'MIME-Version: 1.0' . "\r\n".
-              'Content-type: text/html; charset=utf-8' . "\r\n".
-              'From: ' . $email_from . "\r\n" .
-              'Reply-To: ' . $contactEmail;
-
-  $secretKey = "6LcDGGodAAAAAAn1yEZKDMkKn2kUoZZMRTyln0nr";
-  $responseKey = $_POST['g-recaptcha-response'];
-  $UserIP = $_SERVER['REMOTE_ADDR'];
-  $url = "https://www.google.com/recaptcha/api/siteverify?secret=".$secretKey."&response=".$responseKey."&remoteIP=".$UserIP;
-  $response = file_get_contents($url);
-  $response = json_decode($response);
-
-  if ($response->success)
-  {
-    mail($email_to, $email_subject, $email_body, $headers);
-    echo "
-      <div class='quote-form-notice quote-form-notice-good'>
-        Quote Request Sent Successfully
-      </div>
-      ";
-  }
-  else {
-    echo "
-     <div class='quote-form-notice quote-form-notice-bad'>
-       Oops, something went wrong - please try again;
-      </div>
-      ";
-  }
-};
-?>
